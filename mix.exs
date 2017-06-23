110e818bcdea133769de249c7ec5bcb9cd39aa29
Guillaume Bailleul <laibulle@gmail.com>
2017-06-24 00:55:02 +0200

    add doc dependency

--- mix.exs
+++ mix.exs
@@ -47,6 +47,7 @@ defmodule Exncf.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev},
       {:poison, "~> 3.1"},
       {:httpoison, "~> 0.11.2"},
     ]
